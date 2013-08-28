a9fee73c234029a5442a544e9057453dddf8f9c4
juarezna <juarezna@612ce6a5-2955-e72c-2b63-17e7dcbe4805>
2013-08-28 12:01:43 +0000

    Change project

--- trunk/src/br/com/athustech/gwtexcel/client/ClientSRVAsync.java
+++
@@ -1,8 +0,0 @@
-package br.com.athustech.gwtexcel.client;
-
-
-public interface ClientSRVAsync {
-	
-//	void listarUsuario(String usuario, int pagina, AsyncCallback<List<UsuarioENT>> callback) throws IllegalArgumentException;
-//	void salvarUsuario(int idUnidade, String ativo, String usuario, String nomeCompleto, String senha, List<PerfilENT> perfilUsuarioENT, List<UnidadeENT> listUnidadeENT, String email, boolean enviarEmail, String descricao, AsyncCallback<String> callback) throws IllegalArgumentException;
-}
