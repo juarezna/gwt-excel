59703bc1cd5490c35a65be49b50a373b76c6ea19
juarezna <juarezna@612ce6a5-2955-e72c-2b63-17e7dcbe4805>
2013-08-28 18:39:40 +0000

    Teste implementation.

---
+++ src/com/googlecode/gwtexcel/client/GWTExcel.java
@@ -0,0 +1,13 @@
+package com.googlecode.gwtexcel.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.RootPanel;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class GWTExcel implements EntryPoint {
+	public void onModuleLoad() {
+		RootPanel rootPanel = RootPanel.get();
+	}
+}
