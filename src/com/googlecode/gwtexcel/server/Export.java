d7f4e9496bbf5c332df452234239bca636a16b30
juarezna <juarezna@612ce6a5-2955-e72c-2b63-17e7dcbe4805>
2013-08-29 02:05:00 +0000

    Method noAcent

--- src/com/googlecode/gwtexcel/server/Export.java
+++ src/com/googlecode/gwtexcel/server/Export.java
@@ -8,6 +8,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * Convert GWT tables to Excel.<br>
+ * @author juarezna@gmail.com
+  */
 public class Export extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
